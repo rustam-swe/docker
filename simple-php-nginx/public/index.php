fd9b3853aa7ba6f3be90522f586ef9888a7ae826
Rustam <rustamergashev.sp@gmail.com>
2024-09-30 14:38:24 +0500

    MINOR: Added simple php nginx example

---
+++ simple-php-nginx/public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Website with Nginx running in Docker</title>
+</head>
+<body>
+<h1>
+    <?= "Hello, World from PHP running on Nginx!"; ?>
+</h1>
+<p>Welcome to a simple PHP website running in Docker with Nginx.</p>
+</body>
+</html>
